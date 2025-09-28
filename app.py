ed981d49098d23e6870204ecebd241fc3488aa1b
Hanna Pylieva <pylyeva@gmail.com>
2025-09-28 22:23:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Demo", page_icon="🧠", layout="centered")
 
-st.title("Hello, Streamlit from Colab!")
-name = st.text_input("Ваше ім'я:")
-if name:
-    st.success(f"Привіт, {name}!")
+st.title("My new streamlit app!")
+age = st.text_input("Скільки вам років:")
+if age:
+    st.success(f"Я знаю, що Вам {age} років!")
